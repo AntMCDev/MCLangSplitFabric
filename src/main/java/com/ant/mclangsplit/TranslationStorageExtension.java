417d83b8edea0f5431a4c5caf35b699f10daeb12
AntCasey <antcasey.developer@gmail.com>
2021-10-04 18:20:59 +0100

    Add ignoreKeys "*" wildcards, add includeKeys - which can bypass the ignoreKeys property, add ignoreTooltips option, fix issue with redeclared LanguageSettings table.

---
+++ src/main/java/com/ant/mclangsplit/TranslationStorageExtension.java
@@ -0,0 +1,9 @@
+package com.ant.mclangsplit;
+
+import net.minecraft.client.resource.language.TranslationStorage;
+
+import java.util.HashMap;
+
+public class TranslationStorageExtension {
+    public static TranslationStorage altTranslations = null;
+}
